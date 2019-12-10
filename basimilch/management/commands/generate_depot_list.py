62bffa2762ca08395b7161c1a7881daab5f4f487
flurin-conradin <f.conradin@gmail.com>
2019-12-10 11:13:14 +0100

    sort / remove  imports in generate_depot_list

--- basimilch/management/commands/generate_depot_list.py
+++ basimilch/management/commands/generate_depot_list.py
@@ -1,6 +1,5 @@
-from django.core.management.base import BaseCommand
 from django.core.management import call_command
-import logging
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
