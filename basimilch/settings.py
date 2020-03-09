107f78b4b7152f61da4a4b42f1621c6867d7bc37
flurin-conradin <f.conradin@gmail.com>
2020-03-09 20:42:51 +0100

    add prod to allowed hosts

--- basimilch/settings.py
+++ basimilch/settings.py
@@ -18,6 +18,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "[::1]",
     "basimilch-test.herokuapp.com",
+    "basimilch-prod.herokuapp.com",
     "my.basimil.ch",
     "basimilch.juntagrico.science",
 ]
