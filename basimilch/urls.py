ceb9615f237ee30bc89dd9fb9398a8599f420225
flurin-conradin <f.conradin@gmail.com>
2024-10-20 10:02:41 +0200

    avoid unnecessary regex paths

--- basimilch/urls.py
+++ basimilch/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, re_path
+from django.urls import include, path
 
 urlpatterns = [
-    re_path(r"^admin/", admin.site.urls),
-    re_path(r"^", include("juntagrico_custom_sub.urls")),
-    re_path(r"^", include("juntagrico.urls")),
-    re_path(r"^impersonate/", include("impersonate.urls")),
-    re_path(r"^", include("juntagrico_list_gen.urls")),
-    re_path(r"^", include("juntagrico_depot_management.urls")),
-    re_path(r"^", include("juntagrico_assignment_export.urls")),
-    re_path(r"^", include("juntagrico_polling.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("juntagrico_custom_sub.urls")),
+    path("", include("juntagrico.urls")),
+    path("impersonate/", include("impersonate.urls")),
+    path("", include("juntagrico_list_gen.urls")),
+    path("", include("juntagrico_depot_management.urls")),
+    path("", include("juntagrico_assignment_export.urls")),
+    path("", include("juntagrico_polling.urls")),
 ]
